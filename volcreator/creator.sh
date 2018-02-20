5ef8fbe3538193164216242857b91913149a3dc9
John Strunk <jstrunk@redhat.com>
2018-02-20 13:04:53 -0500

    Fix creator.sh syntax for shellcheck
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- volcreator/creator.sh
+++ volcreator/creator.sh
@@ -23,7 +23,7 @@ function usage() {
 function tohexpath() {
     local -i l1=$1/256
     local -i l2=$1%256
-    printf '%02x/%02x' $l1 $l2
+    printf '%02x/%02x' "$l1" "$l2"
 }
 
 function mkPvTemplate() {
@@ -33,7 +33,8 @@ function mkPvTemplate() {
     local capacity=$4
     local uuid=$5
 
-    local pv_name=$(echo "${uuid}-${subdir}" | tr '/' '-')
+    local pv_name
+    pv_name=$(echo "${uuid}-${subdir}" | tr '/' '-')
     cat - << EOT
 ---
 apiVersion: v1
@@ -41,9 +42,9 @@ kind: PersistentVolume
 metadata:
   name: "$pv_name"
   labels:
-    cluster: "$(echo $servers | tr ':' '-')"
+    cluster: "$(echo "$servers" | tr ':' '-')"
     volume: "$volume"
-    subdir: "$(echo $subdir | tr '/' '-')"
+    subdir: "$(echo "$subdir" | tr '/' '-')"
     supervol: "$uuid"
 spec:
   capacity:
@@ -72,47 +73,43 @@ declare -i i_end=$6
 
 declare -i i=$i_start
 
-if [ $# -ne 6 ]; then usage; exit 1; fi
-if [ $i -lt 0 ]; then usage; exit 1; fi
-if [ $i -gt $i_end ]; then usage; exit 1; fi
-if [ $i_end -gt 65535 ]; then usage; exit 1; fi
+if [ "$#" -ne 6 ]; then usage; exit 1; fi
+if [ "$i" -lt 0 ]; then usage; exit 1; fi
+if [ "$i" -gt "$i_end" ]; then usage; exit 1; fi
+if [ "$i_end" -gt 65535 ]; then usage; exit 1; fi
 
 #-- Make sure quota is enabled on the volume
-gluster volume quota $volume_name enable
+gluster volume quota "$volume_name" enable
 #if [ $? != 0 ]; then
 #    echo "Failed enabling quotas on the volume... continuing anyway."
 #fi
 
 #-- df on a directory should report remaining quota allowance,
 #-- not volume free space.
-gluster volume set $volume_name quota-deem-statfs on
-if [ $? != 0 ]; then
+if ! gluster volume set "$volume_name" quota-deem-statfs on; then
     echo "Failed setting df space reporting volume option... continuing anyway."
 fi
 
-if [ ! -f $base_path/supervol-uuid ]; then
-    uuidgen -r > $base_path/supervol-uuid
+if [ ! -f "$base_path/supervol-uuid" ]; then
+    uuidgen -r > "$base_path/supervol-uuid"
 fi
-supervol_uuid=$(cat $base_path/supervol-uuid)
+supervol_uuid=$(cat "$base_path/supervol-uuid")
 
-rm $base_path/pvs-$i_start-$i_end.yml
-while [ $i -le $i_end ]; do
-    subdir="$(tohexpath $i)"
+rm "$base_path/pvs-$i_start-$i_end.yml"
+while [ "$i" -le "$i_end" ]; do
+    subdir="$(tohexpath "$i")"
     dir="$base_path/$subdir"
     echo "creating: $dir ($i/$i_end)"
-    mkdir -p $dir
-    if [ $? != 0 ]; then
+    if ! mkdir -p "$dir"; then
         echo "Unable to create $dir"
         exit 2
     fi
-    chmod 777 $dir
-    if [ $? != 0 ]; then
+    if ! chmod 777 "$dir"; then
         echo "Unable to set permissions on $dir"
         exit 2
     fi
-    mkPvTemplate $servers $volume_name $subdir "${volsize_gb}Gi" $supervol_uuid >> $base_path/pvs-$i_start-$i_end.yml
-    gluster volume quota $volume_name limit-usage /$subdir ${volsize_gb}GB
-    if [ $? != 0 ]; then
+    mkPvTemplate "$servers" "$volume_name" "$subdir" "${volsize_gb}Gi" "$supervol_uuid" >> "$base_path/pvs-$i_start-$i_end.yml"
+    if ! gluster volume quota "$volume_name" limit-usage "/$subdir" "${volsize_gb}GB"; then
         echo -n "Unable to set gluster quota. "
         echo "vol=$volume_name subdir=$subdir"
         exit 2
